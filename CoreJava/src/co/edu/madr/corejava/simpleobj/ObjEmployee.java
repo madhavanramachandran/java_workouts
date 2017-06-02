8194ed4f05405b0465e265cb39cbab93e5a9ed2f
madhavanram23 <maddy.srv@gmail.com>
2017-06-03 01:09:29 +0530

    Object creation

---
+++ CoreJava/src/co/edu/madr/corejava/simpleobj/ObjEmployee.java
@@ -0,0 +1,52 @@
+package co.edu.madr.corejava.simpleobj;
+
+public class ObjEmployee {
+	
+	public ObjEmployee() {
+		
+	}
+			
+	public ObjEmployee(String eName, long ePhoneNo, long eId) {
+		super();
+		this.eName = eName;
+		this.ePhoneNo = ePhoneNo;
+		this.eId = eId;
+	}
+	
+	private String eName;
+
+	private long ePhoneNo;
+	
+	private long eId;
+	
+	public String geteName() {
+		return eName;
+	}
+
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+
+	public long getePhoneNo() {
+		return ePhoneNo;
+	}
+
+	public void setePhoneNo(long ePhoneNo) {
+		this.ePhoneNo = ePhoneNo;
+	}
+
+	public long geteId() {
+		return eId;
+	}
+
+	public void seteId(long eId) {
+		this.eId = eId;
+	}
+
+	@Override
+	public String toString() {
+		return "ObjEmployee [eName=" + eName + ", ePhoneNo=" + ePhoneNo + ", eId=" + eId + "]";
+	}
+
+	
+}
---
+++ CoreJava/src/co/edu/madr/corejava/simpleobj/ObjMainTest.java
@@ -0,0 +1,20 @@
+package co.edu.madr.corejava.simpleobj;
+
+public class ObjMainTest {
+
+	public static void main(String[] args) {
+		
+		ObjEmployee emp1 = new ObjEmployee("Construtor Method", 1254l, 1001l);
+		
+		ObjEmployee emp2 = new ObjEmployee();
+		emp2.seteId(1002l);
+		emp2.seteName("Seperate setter");
+		emp2.setePhoneNo(57565l);
+		
+		System.out.println("Setting values by construtor:: " + emp1);
+		
+		System.out.println("Setting values by seperate method:: " + emp2);
+
+	}
+
+}
