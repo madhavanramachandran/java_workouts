eed5da10db25ecbfc8e6b58641ea369721c696a4
madhavanram23 <maddy.srv@gmail.com>
2018-11-22 22:04:24 +0530

    Daily coding problem 22Nov2018

---
+++ CoreJava/src/co/edu/madr/codinginterview/chapter3/StackOfPlates_3_3.java
@@ -0,0 +1,44 @@
+package co.edu.madr.codinginterview.chapter3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class StackOfPlates_3_3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+}
+
+class SetOfStack {
+	Map<Integer, Stack<Integer>> stackMap = new HashMap<>();
+	
+	final int maxPerStackSize = 2;
+	
+	int stackNum;
+	
+	public SetOfStack() {
+		stackMap.put(0, new Stack<Integer>());
+		stackNum = 0;
+	}
+	
+	public void push(int data) {
+		stackMap.get(stackNum).push(data);
+		int currSize = stackMap.get(stackNum).size();
+		if (currSize == maxPerStackSize) {
+			stackMap.put(++stackNum, new Stack<>());
+		}
+	}
+	
+	public Integer pop() {
+		return popAt(stackNum);
+	}
+	
+	public Integer popAt(int index) {
+		return stackMap.get(index).pop();
+	}
+	
+}
---
+++ CoreJava/src/co/edu/madr/daillycoding/nov2018/ExcludeSelf_22_11_2018.java
@@ -0,0 +1,83 @@
+package co.edu.madr.daillycoding.nov2018;
+
+import java.util.Arrays;
+
+/**
+ * This problem was asked by Uber.
+ * 
+ * Given an array of integers, return a new array such that each element at
+ * index i of the new array is the product of all the numbers in the original
+ * array except the one at i.
+ * 
+ * For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+ * [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would
+ * be [2, 3, 6].
+ * 
+ * Follow-up: what if you can't use division?
+ * 
+ * @author madr
+ *
+ */
+public class ExcludeSelf_22_11_2018 {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5 };
+		excludeSelf(arr);
+		excludeSelfNoDivision(arr);
+	}
+
+	/**
+	 * Time Complexity: O(N) [N + N]
+	 * 
+	 * @param arr
+	 */
+	static void excludeSelf(int[] arr) {
+		int prod = 1;
+		for (int i = 0; i < arr.length; i++) {
+			prod *= arr[i];
+		}
+
+		int[] result = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			result[i] = prod / arr[i];
+		}
+		System.out.println("Result:: " + Arrays.toString(result));
+	}
+
+	/**
+	 * Time COmplexity: O(N) [N + N + N]
+	 * 
+	 * THis is one with no division is involved.
+	 * 
+	 * @param arr
+	 */
+	static void excludeSelfNoDivision(int[] arr) {
+		int[] preMul = new int[arr.length];
+		int[] postMul = new int[arr.length];
+
+		preMul[0] = 1;
+		postMul[arr.length - 1] = 1;
+
+		int preProd = 1;
+		int postProd = 1;
+		for (int i = 1; i < arr.length; i++) {
+			preProd *= arr[i - 1];
+			preMul[i] = preProd;
+		}
+
+		for (int i = arr.length - 2; i >= 0; i--) {
+			postProd *= arr[i + 1];
+			postMul[i] = postProd;
+		}
+
+		int[] result = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			result[i] = preMul[i] * postMul[i];
+		}
+
+		System.out.println(Arrays.toString(preMul));
+		System.out.println(Arrays.toString(postMul));
+		System.out.println("Result:: " + Arrays.toString(result));
+	}
+
+}
